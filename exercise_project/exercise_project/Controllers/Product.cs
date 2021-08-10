d532d1ebb7b58e98576dcea562a7ec21bbbaa6ab
umuttosun23 <81216369+umuttosun23@users.noreply.github.com>
2021-08-10 18:03:41 +0300

    exercise_project

---
+++ exercise_project/exercise_project/Controllers/Product.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using exercise_project.Models;
+namespace exercise_project.Controllers
+{
+    public class Product : Controller
+    {
+        Context context = new Context();
+        public IActionResult Index()
+        {
+            var degerler = context.Products.ToList();
+            return View(degerler);
+        }
+    }
+}
---
+++ exercise_project/exercise_project/Models/Context.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace exercise_project.Models
+{
+    public class Context : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=GBPinG\\SQLEXPRESS;database=e-commerce;integrated security=true;");
+        }
+        public DbSet<Kategories> Kategories { get; set; }
+        public DbSet<Products> Products { get; set; }
+    }
+}
